feef819811f043624ef2c7780950fcacf61e88af
Wenchao Wang <unlock.liberty@gmail.com>
2014-03-03 16:17:04 -0500

    Update FuelCan.cs

--- CrashingPlane/CrashingPlane/Entities/FuelCan.cs
+++ CrashingPlane/CrashingPlane/Entities/FuelCan.cs
@@ -9,15 +9,27 @@ namespace CrashingPlane.Entities
 {
     class FuelCan : BasicEntity
     {
-        public FuelCan()
-            : base(ContentHolder.FuelCanTexture)
-        { }
+        float rotation; // we want the texture to rotate 
+        
+        public FuelCan(Vector2 position, float velocityX)
+            : base(ContentHolder.FuelCanTexture, new Point(20, 20))
+        {
+            // TODO: Change collisionSize according to the spritesheet
+            this.position = position;
+            this.velocity = new Vector2(velocityX, 0);
+        }
 
         public override void Trigger(Chopper player)
         {
             if (player.IsAlive)
                 player.AddFuel(20);
             base.Trigger(player);
+        }
+        
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            // Override this because we want to draw with rotation
+            
         }
     }
 }
